1667fbe348de17bdc89c20fd788c3523c29356f2
Bim3264 <anawatchbim3264@gmail.com>
2014-12-09 22:44:53 +0700

    Added Texture

--- src/optimum2d/mapeditor/MapEditor.java
+++ src/optimum2d/mapeditor/MapEditor.java
@@ -1,16 +1,7 @@
 package optimum2d.mapeditor;
 
-import optimum2d.graphics.shape.Box;
 import optimum2d.main.Game;
 import optimum2d.main.Updater;
-import optimum2d.rendering.RenderingEngine;
-import optimum2d.util.GameLogger;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.util.vector.Vector2f;
-
-import java.util.ArrayList;
 
 /**
  * Created by Biw on 1/12/2557.
